c20237d9789a9bb4689443ef9bbba7e540f697e5
JennyMacdd <jenm99@gmail.com>
2022-08-24 23:02:40 +0100

    Moving api call so only triggered on submit

--- github-user-search-react-app/src/components/searchComponent.tsx
+++ github-user-search-react-app/src/components/searchComponent.tsx
@@ -1,36 +1,44 @@
 import React from 'react';
 import UserComponent from './userComponent';
+import { AppService } from '../services/app.service';
 
 type myProps = {
 
 }
 
 type myState = {
-    username: string
+    username: string,
+    userData: userData
+}
+
+type userData = {
+    url: string
 }
 
 class SearchComponent extends React.Component<myProps, myState> {
     constructor(props: myProps) {
         super(props);
         this.state = {
-            username: ''
+            username: '',
+            userData: { url: ''}
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e: React.FormEvent<HTMLInputElement>) { this.setState({ username: e.currentTarget.value }); }
-    handleSubmit(e: React.FormEvent<HTMLInputElement>) {
-        alert('A name was submitted: ' + this.state.username);
-        e.preventDefault();
+    async handleSubmit() {
+        const appService = new AppService();
+        const getCurrentUser = await appService.getUser(this.state.username);
+        this.setState({ userData: getCurrentUser })
     }
 
     render() {
         return (
             <div className="searchComponent">
                 <input type="text" placeholder="Enter a username" value={this.state.username} onChange={this.handleChange} />
-                <input type="submit" value="Submit" /> 
-                <UserComponent username={this.state.username} />
+                <input type="submit" value="Submit" onClick={this.handleSubmit} />
+                <UserComponent userData={this.state.userData} />
             </div>
         )
     }
